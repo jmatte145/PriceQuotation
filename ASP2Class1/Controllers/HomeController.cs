7e42e788246f0cef065db0372fde125ccf3a9444
Justin Matte <justin.matte145@gmail.com>
2021-09-02 21:02:47 -0400

    Add project files.

---
+++ ASP2Class1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PriceQuotation.Models;
+
+namespace PriceQuotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Discount = 0;
+            ViewBag.Price = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(SubtotalModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Discount = model.CalculateDiscount();
+                ViewBag.Price = model.CalculatePrice();
+            }
+            else
+            {
+                ViewBag.Discount = 0;
+                ViewBag.Price = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ ASP2Class1/Models/SubtotalModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceQuotation.Models
+{
+    using System.ComponentModel.DataAnnotations;
+        public class SubtotalModel
+        {
+            [Required(ErrorMessage = "Please enter a valid subtotal.")]
+            public decimal? Subtotal { get; set; }
+            
+            [Range (0,100, ErrorMessage = "Discount must be between 0 and 100%.")]
+            [Required(ErrorMessage = "Please enter a valid discount percentage.")]
+            public decimal? DiscountPercent { get; set; }
+
+            public decimal? DiscountAmount { get; set; }
+
+            public decimal? Total { get; set; }
+            
+            public decimal? CalculateDiscount()
+            {
+                decimal? discTotal = 0;
+                discTotal = (Subtotal * (DiscountPercent/100));
+                return discTotal;
+            }
+            public decimal? CalculatePrice()
+            {
+                decimal? total = 0;
+                total = Subtotal - (Subtotal * (DiscountPercent/100));
+                return total;
+            }
+        }
+}
